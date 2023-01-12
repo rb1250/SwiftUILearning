faef97e09c517a0a131398dbf75cc98b7dec432b
rb1250 <rb1250@users.noreply.github.com>
2023-01-12 01:07:55 -0500

    SwiftUI First App

---
+++ swiftUIApp/swiftUIApp/swiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIAppApp.swift
+//  swiftUIApp
+//
+//  Created by Ruchika Bokadia on 11/01/23.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
